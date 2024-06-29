7cc817259e88f71287c235e6bef8865c5330ffd7
Adilcpgithub <adilcp8590@email.com>
2024-06-29 21:14:22 +0530

    netflix fist page completed

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/models/topratedtv.dart';
 import 'package:netflix_clone/models/upcoming_movie.dart';
 import 'package:netflix_clone/services/api_services.dart';
+import 'package:netflix_clone/widgets/custom_carousel.dart';
+import 'package:netflix_clone/widgets/movie_card_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,50 +14,77 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<UpcomingMovieModel> upcomingFuture;
+  late Future<UpcomingMovieModel> nowPlayingFuture;
+  late Future<TopRatedTvShowsModel> topRated;
+
   ApiServices apiServices = ApiServices();
   @override
   void initState() {
     upcomingFuture = apiServices.getUpcomingMovies();
+    nowPlayingFuture = apiServices.getNowPlayingMovies();
+    topRated = apiServices.getTopRatedShows();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.asset(
-          'assets/logo.png',
-          height: 50,
-          width: 120,
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: InkWell(
-                onTap: () {},
-                child: const Icon(
-                  size: 30,
-                  Icons.search,
-                  color: Colors.white,
-                )),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.asset(
+            'assets/logo.png',
+            height: 50,
+            width: 120,
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(6),
-            child: Container(
-              color: Colors.blue,
-              height: 27,
-              width: 27,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: InkWell(
+                  onTap: () {},
+                  child: const Icon(
+                    size: 30,
+                    Icons.search,
+                    color: Colors.white,
+                  )),
             ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Container(
+                color: Colors.blue,
+                height: 27,
+                width: 27,
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              FutureBuilder(
+                  future: topRated,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return CustomCurouselSlider(data: snapshot.data!);
+                    } else {
+                      return SizedBox.shrink();
+                    }
+                  }),
+              SizedBox(
+                height: 220,
+                child: MovieCardWidget(
+                    future: upcomingFuture, headLineText: 'Upcoming Movies'),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 220,
+                child: MovieCardWidget(
+                    future: nowPlayingFuture, headLineText: 'Now Playing'),
+              )
+            ],
           ),
-          const SizedBox(
-            width: 20,
-          )
-        ],
-      ),
-      body: Center(
-        child: Text('home screen'),
-      ),
-    );
+        ));
   }
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
-import 'dart:math';
-
-import 'package:netflix_clone/common/utils.dart';
-import 'package:netflix_clone/models/upcoming_movie.dart';
 import 'package:http/http.dart' as http;
+import 'package:netflix_clone/models/topratedtv.dart';
+import 'package:netflix_clone/models/upcoming_movie.dart';
+import 'package:netflix_clone/common/utils.dart';
 
 const baseUrl = "https://api.themoviedb.org/3/";
 var key = "?api_key=$apiKey";
@@ -15,10 +14,33 @@ class ApiServices {
     final url = "$baseUrl$endPoint$key";
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      log("Success" as num);
+      print("Success ");
       return UpcomingMovieModel.fromJson(
-          jsonEncode(response.body) as Map<String, dynamic>);
+          jsonDecode(response.body) as Map<String, dynamic>);
     }
-    throw Exception('Falied to load upcoming movies');
+    throw Exception('Failed to load upcoming movies');
+  }
+
+  Future<UpcomingMovieModel> getNowPlayingMovies() async {
+    endPoint = "movie/now_playing";
+    final url = "$baseUrl$endPoint$key";
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print("Success ");
+      return UpcomingMovieModel.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
+    }
+    throw Exception('Failed to load now playing movies');
+  }
+
+  Future<TopRatedTvShowsModel> getTopRatedShows() async {
+    endPoint = "tv/top_rated";
+    final url = "$baseUrl$endPoint$key";
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      print("Success ");
+      return TopRatedTvShowsModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load upcomeing movies");
   }
 }
